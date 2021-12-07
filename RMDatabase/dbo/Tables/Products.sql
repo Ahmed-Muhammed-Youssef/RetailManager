090ac2fb06d7034d1bcbc7c8a6cff5958a71aa5f
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2021-12-07 21:33:24 +0200

    Register Database Tables
    The tables for the basic register system

---
+++ RMDatabase/dbo/Tables/Products.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Products]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[ProductName] NVARCHAR(100) NOT NULL, 
+	[Description] NVARCHAR(600) NOT NULL, 
+	[RetailPrice] NCHAR(10) NOT NULL,
+	[CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+	[LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
---
+++ RMDatabase/dbo/Tables/SalesDetails.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SalesDetails]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+	[SaleId] INT NOT NULL, 
+	[ProductId] NCHAR(10) NOT NULL, 
+	[Quantity] INT NOT NULL DEFAULT 1,
+	[PurchasePrice] MONEY NOT NULL, 
+	[Tax] MONEY NOT NULL DEFAULT 0, 
+)
