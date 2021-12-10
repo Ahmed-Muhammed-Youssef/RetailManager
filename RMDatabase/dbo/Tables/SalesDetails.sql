5c6edf77ff28ba1b4e2c94d7ac82231dcc4e3c52
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2021-12-10 19:02:02 +0200

    Add Foreign Keys Relationships
    Adds Foreign keys relationships to tables

--- RMDatabase/dbo/Tables/SalesDetails.sql
+++ RMDatabase/dbo/Tables/SalesDetails.sql
@@ -2,8 +2,10 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
 	[SaleId] INT NOT NULL, 
-	[ProductId] NCHAR(10) NOT NULL, 
+	[ProductId] INT NOT NULL, 
 	[Quantity] INT NOT NULL DEFAULT 1,
 	[PurchasePrice] MONEY NOT NULL, 
 	[Tax] MONEY NOT NULL DEFAULT 0, 
+	CONSTRAINT [FK_SalesDetails_ToSales] FOREIGN KEY ([SaleId]) REFERENCES [Sales]([Id]), 
+	CONSTRAINT [FK_SalesDetails_ToProducts] FOREIGN KEY ([ProductId]) REFERENCES [Products]([Id]) 
 )
