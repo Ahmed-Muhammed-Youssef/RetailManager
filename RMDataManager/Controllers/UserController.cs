cfbd75aa8d7a7c9d760a18bd9fcc87ee01a9f7c8
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2021-12-16 00:44:59 +0200

    Add API To Diplay All Users
    Adds an API to display all users (id, email, roles),
    and it's only accessible to users with thee admin role.

--- RMDataManager/Controllers/UserController.cs
+++ RMDataManager/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using RMDataManager.Library.DataAccess.Internal.DataAccess;
 using RMDataManager.Library.Models;
+using RMDataManager.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,17 +11,57 @@ using System.Web.Http;
 namespace RMDataManager.Controllers
 {
     [Authorize]
-    [RoutePrefix("api/User")]
+    [RoutePrefix("api")]
     public class UserController : ApiController
     {
         // GET: User/Id/
         [HttpGet]
+        [Route("userid")]
         public UserModel GetUser()
         {
             string id = RequestContext.Principal.Identity.GetUserId();
             UserData userData = new UserData();
             
             return userData.GetUserById(id).First();
+        }
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [Route("admin/users")]
+        public List<ToDisplayUserModel> GetAllUsers()
+        {
+            List<ToDisplayUserModel> toDisplayUserModels = new List<ToDisplayUserModel>();
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                var users = userManager.Users.ToList();
+                var roles = context.Roles.ToList();
+
+                foreach (var user in users)
+                {
+                    var userToDisplay = new ToDisplayUserModel()
+                    {
+                        Id = user.Id,
+                        Email = user.Email
+                    };
+                    foreach (var userRole in user.Roles)
+                    {
+                        //To get the role name corresponding to the role id
+                        foreach (var role in roles)
+                        {
+                            if (role.Id == userRole.RoleId)
+                            {
+                                userToDisplay.Roles.Add(role.Id, role.Name);
+                            }
+
+                        }
+                    }
+                    toDisplayUserModels.Add(userToDisplay);
+                }
+            }
+
+            return toDisplayUserModels;
         }
     }
 }
